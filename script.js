11fb948a2033e6d7648f95db56305b4e9b84a3d3
pwrignall <pwrignall@users.noreply.github.com>
2024-06-11 22:18:52 +0100

    Populate fixtures

--- script.js
+++ script.js
@@ -3,20 +3,11 @@ const fixtures = [
     date: "2024-06-14",
     fixtures: [
       {
-        team1: "Turkey",
-        team2: "Croatia",
+        team1: "Germany",
+        team2: "Scotland",
         stage: "Group A",
-        venue: "Stadio Olimpico",
-        time: "21:00",
-        score: "0-0",
-        extraInfo: "Turkey win 3-2 on pens",
-      },
-      {
-        team1: "Poland",
-        team2: "Sweden",
-        stage: "Group B",
-        venue: "Aviva Stadium",
-        time: "18:00",
+        venue: "Munich",
+        time: "19:00",
         score: "",
         extraInfo: "",
       },
@@ -26,20 +17,552 @@ const fixtures = [
     date: "2024-06-15",
     fixtures: [
       {
-        team1: "France",
-        team2: "Portugal",
+        team1: "Hungary",
+        team2: "Switzerland",
+        stage: "Group A",
+        venue: "Cologne",
+        time: "13:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Spain",
+        team2: "Croatia",
+        stage: "Group B",
+        venue: "Berlin",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Italy",
+        team2: "Albania",
+        stage: "Group B",
+        venue: "Dortmund",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-16",
+    fixtures: [
+      {
+        team1: "Poland",
+        team2: "Netherlands",
+        stage: "Group D",
+        venue: "Hamburg",
+        time: "13:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Slovenia",
+        team2: "Denmark",
         stage: "Group C",
-        venue: "Allianz Arena",
-        time: "21:00",
+        venue: "Stuttgart",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Serbia",
+        team2: "England",
+        stage: "Group C",
+        venue: "Gelsenkirchen",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-17",
+    fixtures: [
+      {
+        team1: "Romania",
+        team2: "Ukraine",
+        stage: "Group E",
+        venue: "Munich",
+        time: "13:00",
         score: "",
         extraInfo: "",
       },
       {
         team1: "Belgium",
-        team2: "Netherlands",
+        team2: "Slovakia",
+        stage: "Group E",
+        venue: "Frankfurt",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Austria",
+        team2: "France",
         stage: "Group D",
-        venue: "Wembley Stadium",
-        time: "18:00",
+        venue: "Dusseldorf",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-18",
+    fixtures: [
+      {
+        team1: "Turkey",
+        team2: "Georgia",
+        stage: "Group F",
+        venue: "Dortmund",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Portugal",
+        team2: "Czechia",
+        stage: "Group F",
+        venue: "Leipzig",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-19",
+    fixtures: [
+      {
+        team1: "Croatia",
+        team2: "Albania",
+        stage: "Group B",
+        venue: "Hamburg",
+        time: "13:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Germany",
+        team2: "Hungary",
+        stage: "Group A",
+        venue: "Stuttgart",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Scotland",
+        team2: "Switzerland",
+        stage: "Group A",
+        venue: "Cologne",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-20",
+    fixtures: [
+      {
+        team1: "Slovenia",
+        team2: "Serbia",
+        stage: "Group C",
+        venue: "Munich",
+        time: "13:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Denmark",
+        team2: "England",
+        stage: "Group C",
+        venue: "Frankfurt",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Spain",
+        team2: "Italy",
+        stage: "Group B",
+        venue: "Gelsenkirchen",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-21",
+    fixtures: [
+      {
+        team1: "Slovakia",
+        team2: "Ukraine",
+        stage: "Group E",
+        venue: "Dusseldorf",
+        time: "13:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Poland",
+        team2: "Austria",
+        stage: "Group D",
+        venue: "Berlin",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Netherlands",
+        team2: "France",
+        stage: "Group D",
+        venue: "Leipzig",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-22",
+    fixtures: [
+      {
+        team1: "Georgia",
+        team2: "Czechia",
+        stage: "Group F",
+        venue: "Hamburg",
+        time: "13:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Turkey",
+        team2: "Portugal",
+        stage: "Group F",
+        venue: "Dortmund",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Belgium",
+        team2: "Romania",
+        stage: "Group E",
+        venue: "Cologne",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-23",
+    fixtures: [
+      {
+        team1: "Switzerland",
+        team2: "Germany",
+        stage: "Group A",
+        venue: "Frankfurt",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Scotland",
+        team2: "Hungary",
+        stage: "Group A",
+        venue: "Stuttgart",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-24",
+    fixtures: [
+      {
+        team1: "Croatia",
+        team2: "Italy",
+        stage: "Group B",
+        venue: "Leipzig",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Albania",
+        team2: "Spain",
+        stage: "Group B",
+        venue: "Dusseldorf",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-25",
+    fixtures: [
+      {
+        team1: "Netherlands",
+        team2: "Austria",
+        stage: "Group D",
+        venue: "Berlin",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "France",
+        team2: "Poland",
+        stage: "Group D",
+        venue: "Dortmund",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "England",
+        team2: "Slovenia",
+        stage: "Group C",
+        venue: "Cologne",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Denmark",
+        team2: "Serbia",
+        stage: "Group C",
+        venue: "Munich",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-26",
+    fixtures: [
+      {
+        team1: "Slovakia",
+        team2: "Romania",
+        stage: "Group E",
+        venue: "Frankfurt",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Ukraine",
+        team2: "Belgium",
+        stage: "Group E",
+        venue: "Stuttgart",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Czechia",
+        team2: "Turkey",
+        stage: "Group F",
+        venue: "Hamburg",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Georgia",
+        team2: "Portugal",
+        stage: "Group F",
+        venue: "Gelsenkirchen",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-29",
+    fixtures: [
+      {
+        team1: "2nd Group A",
+        team2: "2nd Group B",
+        stage: "Round of 16",
+        venue: "Berlin",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "1st Group A",
+        team2: "2nd Group C",
+        stage: "Round of 16",
+        venue: "Dortmund",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-06-30",
+    fixtures: [
+      {
+        team1: "1st Group C",
+        team2: "3rd Group D/E/F",
+        stage: "Round of 16",
+        venue: "Gelsenkirchen",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "1st Group B",
+        team2: "3rd Group A/D/E/F",
+        stage: "Round of 16",
+        venue: "Cologne",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-01",
+    fixtures: [
+      {
+        team1: "2nd Group D",
+        team2: "2nd Group E",
+        stage: "Round of 16",
+        venue: "Dusseldorf",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "1st Group F",
+        team2: "3rd Group A/B/C",
+        stage: "Round of 16",
+        venue: "Frankfurt",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-02",
+    fixtures: [
+      {
+        team1: "1st Group E",
+        team2: "3rd Group A/B/C/D",
+        stage: "Round of 16",
+        venue: "Munich",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "1st Group D",
+        team2: "2nd Group F",
+        stage: "Round of 16",
+        venue: "Leipzig",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-05",
+    fixtures: [
+      {
+        team1: "Winner R16 #4",
+        team2: "Winner R16 #2",
+        stage: "Quarter-finals",
+        venue: "Stuttgart",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Winner R16 #6",
+        team2: "Winner R16 #5",
+        stage: "Quarter-finals",
+        venue: "Hamburg",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-06",
+    fixtures: [
+      {
+        team1: "Winner R16 #1",
+        team2: "Winner R16 #3",
+        stage: "Quarter-finals",
+        venue: "Dusseldorf",
+        time: "16:00",
+        score: "",
+        extraInfo: "",
+      },
+      {
+        team1: "Winner R16 #7",
+        team2: "Winner R16 #8",
+        stage: "Quarter-finals",
+        venue: "Berlin",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-09",
+    fixtures: [
+      {
+        team1: "Winner QF #1",
+        team2: "Winner QF #2",
+        stage: "Semi-finals",
+        venue: "Munich",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-10",
+    fixtures: [
+      {
+        team1: "Winner QF #3",
+        team2: "Winner QF #4",
+        stage: "Semi-finals",
+        venue: "Dortmund",
+        time: "19:00",
+        score: "",
+        extraInfo: "",
+      },
+    ],
+  },
+  {
+    date: "2024-07-14",
+    fixtures: [
+      {
+        team1: "Winner SF #1",
+        team2: "Winner SF #2",
+        stage: "Euro 2024 final",
+        venue: "Berlin",
+        time: "19:00",
         score: "",
         extraInfo: "",
       },
@@ -51,17 +574,24 @@ const checkbox = document.getElementById("swap-checkbox");
 const timeOrScoreSpans = document.getElementsByClassName("time-score");
 const extraInfoSpans = document.getElementsByClassName("extra-info");
 
-function swapMatchTimeWithScore() {
-  for (let i = 0; i < timeOrScoreSpans.length; i++) {
-    const span = timeOrScoreSpans[i];
-    const match = fixtures[(i / 2) | 0].fixtures[i % 2];
-    const extraInfoSpan = extraInfoSpans[i];
+const matches = fixtures.flatMap((dateItem) =>
+  dateItem.fixtures.map((match) => ({ ...match, date: dateItem.date }))
+);
+
+function displayScoresAndExtraInfo() {
+  for (let i = 0; i < matches.length; i++) {
     if (checkbox.checked) {
-      span.textContent = match.score;
-      extraInfoSpan.textContent = match.extraInfo;
+      if (matches[i].score !== "") {
+        timeOrScoreSpans[i].textContent = matches[i].score;
+      }
+      extraInfoSpans[i].textContent = matches[i].extraInfo;
     } else {
-      span.textContent = match.time;
-      extraInfoSpan.textContent = "";
+      const dateTime = new Date(`${matches[i].date}T${matches[i].time}Z`);
+      timeOrScoreSpans[i].textContent = dateTime
+        .toLocaleTimeString("en-GB")
+        .slice(0, -3)
+        .replace(",", "");
+      extraInfoSpans[i].textContent = "";
     }
   }
 }
@@ -70,12 +600,14 @@ const fixturesList = document.getElementById("fixtures-list");
 
 fixtures.forEach((dateItem) => {
   const date = document.createElement("h2");
-  date.textContent = new Date(dateItem.date).toLocaleDateString("en-GB", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  date.textContent = new Date(dateItem.date)
+    .toLocaleDateString("en-GB", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })
+    .replace(",", "");
   fixturesList.appendChild(date);
   const ul = document.createElement("ul");
   fixturesList.appendChild(ul);
